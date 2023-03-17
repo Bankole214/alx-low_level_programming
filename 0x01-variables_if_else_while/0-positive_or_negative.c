558f6d6ff015bfa9a48b229ff3c401dd533171fb
Bankole214 <adegunlenurudeen214@gmail.com>
2023-03-17 16:16:33 -0700

    generate numbers and print with if statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
 #include <stdio.h>
-
 /* betty style doc for function main goes there */
+
 /**
- * main - Determines if a number is positive, negative or zero.
- *
- * Return: Always 0 (Success)
+ * main - main code startes here
+ * Return: Always 0 success
  */
+
 int main(void)
 {
 	int n;
@@ -19,7 +20,7 @@ int main(void)
 		printf("%d is positive", n);
 	else if (n == 0)
 		printf("%d is zero", n);
-        else if (n < 0)
+	else if (n < 0)
 		printf("%d is negative", n);
 	printf("\n");
 	return (0);
