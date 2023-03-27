d50bc754468a71eb7c8eabfdcba75d5f417931e9
Bankole214 <adegunlenurudeen214@gmail.com>
2023-03-27 15:54:30 -0700

    override the value and print another output

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 - function that takes a pointer ant output an int
+* @n: input n
+* Return: return value of n
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
