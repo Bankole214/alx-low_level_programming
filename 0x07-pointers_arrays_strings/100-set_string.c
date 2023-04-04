f736a0ae1d253a581d6ac0d4546eb5edc314f892
Bankole214 <adegunlenurudeen214@gmail.com>
2023-04-03 18:38:36 -0700

    deference a string to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* set_string - deference a string to a character
+* @s: string
+* @to: destination
+* Return: always 0
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
