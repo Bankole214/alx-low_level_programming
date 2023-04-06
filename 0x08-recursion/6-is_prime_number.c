4a360232c7c7e17e810ae9ede4028c572dc111c8
Bankole214 <adegunlenurudeen214@gmail.com>
2023-04-05 19:14:08 -0700

    function that print actual prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+int actual_prime_number(int n, int i);
+
+/**
+* is_prime_number - function that return prime number
+* @n: input
+* Return: 1 if n is prime else 0
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime_number(n, n - 1));
+}
+/**
+* actual_prime_number - check if n is prime and recursive
+* @n: input
+* @i: iterator
+* Return: 1 if n is prime else 0
+*/
+int actual_prime_number(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime_number(n, i - 1));
+}
